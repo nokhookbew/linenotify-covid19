167b1757a6c434ebf29ed08d647c411fdc3a9add
Bewvy Worapat <nokhook_bew@hotmail.com>
2020-04-16 21:47:37 +0700

    fixed bug and modiflied try cast and add function send()

--- noti-covid19.py
+++ noti-covid19.py
@@ -8,6 +8,14 @@ current_healed = 0
 current_healing = 0
 current_die = 0
 
+def send(format_data, now):
+    token = '<token form linenotify>'
+    url = 'https://notify-api.line.me/api/notify'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + token}
+    r = requests.post(url, headers=headers, data={'message': format_data})
+    print(r.content, now)
+
 def noti():
     global current_infected
     global current_healed
@@ -24,25 +32,21 @@ def noti():
         new_healing = int(data['Hospitalized'])
         new_die = int(data['Deaths'])
         format_data = ''
+        if new_infected != current_infected or new_healed != current_healed or new_healing != current_healing or new_die != current_die:
+            for key, value in data.items():
+                format_data += key + ': ' + str(value) + '\n'
+            send(format_data, now)
+            current_infected = new_infected
+            current_healed = new_healed
+            current_healing = new_healing
+            current_die = new_die
+        else:
+            print('ยอดเท่าเดิม', now)
+        print(current_infected)
     except Exception as e:
         print(e)
         format_data = 'api responses somethin wrong'
-    token = '<token form linenotify>'
-    url = 'https://notify-api.line.me/api/notify'
-    headers = {'Content-Type': 'application/x-www-form-urlencoded',
-              'Authorization': 'Bearer ' + token}
-    if new_infected != current_infected or new_healed != current_healed or new_healing != current_healing or new_die != current_die:
-        for key, value in data.items():
-            format_data += key + ': ' + str(value) + '\n'
-        r = requests.post(url, headers=headers, data={'message': format_data})
-        current_infected = new_infected
-        current_healed = new_healed
-        current_healing = new_healing
-        current_die = new_die
-        print(r.content, now)
-    else:
-        print('ยอดเท่าเดิม', now)
-    print(current_infected)
+        send(format_data, now)
 
 print('running...............')
 noti()
